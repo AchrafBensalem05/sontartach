39dbb6e086a93f8e4340a766cb52eb36aab51ddb
AchrafBensalem05 <ac.bensalem@esi-sba.dz>
2024-07-01 11:58:35 +0100

    notificaiton

--- app/models/notification.js
+++ app/models/notification.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 
 const NotificationSchema = new mongoose.Schema({
   message: String,
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true,},
   createdAt: {
     type: Date,
     default: Date.now,
